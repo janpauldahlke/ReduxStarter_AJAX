7c6e197a887be8571fb00b5fa6b4b4341d3ba73b
janpauldahlke <janpauldahlke@gmail.com>
2017-03-20 11:47:38 +0100

    event bind searchbar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state= { term: ''};
+  }
+
+  //pass (vanilla) event handler
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render () {
     return(
       <form className="input-group">
         <p>lorem ipsum from src/containers/search_bar.js</p>
-        <input />
+        <input
+          placeholder="forecast your city"
+          className="form-controll"
+          value={ this.state.term }
+          onChange={ this.onInputChange }
+          />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">
             search
@@ -16,3 +32,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+
+/*TODO
+  refactor form into a controlled field, where
+  value of input is set by state of the Component
+*/
