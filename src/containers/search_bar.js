92c0e043dcad0430d999cdfad82b8c6ee0adc612
janpauldahlke <janpauldahlke@gmail.com>
2017-03-20 12:20:23 +0100

    esoteric this bindings

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -6,20 +6,41 @@ export default class SearchBar extends Component {
     super(props);
 
     this.state= { term: ''};
+
+    //bind context of onInputChange
+                          //right handed this is instance of SearchBar
+    //replace this of method with new instance this
+    //esoteric sh1t!
+    /*
+      1. take the existing function
+      2. bind this
+      3. replace left handed
+      => we kind of overwrting this local method here
+
+      //wieso???
+
+      https://stackoverflow.com/questions/42902167/bindings-can-someone-help-me-to-get-this-right
+    */
+
+
+
+    this.onInputChange = this.onInputChange.bind(this);
   }
 
   //pass (vanilla) event handler
   onInputChange(event) {
+
     console.log(event.target.value);
+    this.setState({ term: event.target.value});
   }
 
   render () {
     return(
       <form className="input-group">
-        <p>lorem ipsum from src/containers/search_bar.js</p>
+
         <input
           placeholder="forecast your city"
-          className="form-controll"
+          className="form-control"
           value={ this.state.term }
           onChange={ this.onInputChange }
           />
